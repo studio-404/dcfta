7759b570f71ab9062a2a04a22b6c3dc227f5bda6
gio <hosting@404.ge>
2016-11-13 15:43:08 +0400

    add style js font files

---
+++ public/markup/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<title>DCFTA</title>
+	<script src="js/jquery-3.1.1.min.js" type="text/css"></script>
+	<script src="js/materialize.min.js" type="text/css"></script>
+	<script src="js/script.js" type="text/css"></script>
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+Hi
+</body>
+</html>
